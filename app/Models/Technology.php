08cafa487e027e43bdb6c9eb883c4ac6660d0bde
DanielMarella <marella.daniel96@gmail.com>
2023-08-28 18:35:34 +0200

    defined the many-to-many relationship with the model Project

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Admin\Project;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Technology extends Model
 {
     use HasFactory;
+    
+    public function projects(){
+        return $this->belongsToMany(Project::class)
+    }
 }
